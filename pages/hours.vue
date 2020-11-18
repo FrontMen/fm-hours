e662fa9e1b1ab7bcabae47bfa40dff1b4f1eb1f3
Bart Haasbroek <haasbroek_89@hotmail.com>
2020-11-18 14:36:48 +0100

    Add possibility to navigate between weeks

--- pages/hours.vue
+++ pages/hours.vue
@@ -4,39 +4,26 @@
         <b-container fluid="lg">
             <div class="hours-table__date">
                 <div class="hours-table__date-nav date-nav mr-3">
-                    <b-button>
+                    <b-button @click="prevWeek()">
                         <b-icon icon="arrow-left"></b-icon>
                     </b-button>
-                    <b-button>
+                    <b-button @click="nextWeek()">
                         <b-icon icon="arrow-right"></b-icon>
                     </b-button>
                 </div>
-                {{currentWeekLabel}}
+                {{weekLabel}}
             </div>
             <b-row class="hours-table__top-row text-center">
                 <b-col></b-col>
                 <b-col>
                     <b-container class="p-0 d-none d-md-block">
                         <b-row class="text-center">
-                            <b-col>
-                                Mo
-                                <div class="date">05 oct</div>
-                            </b-col>
-                            <b-col>
-                                Tu
-                                <div class="date">06 oct</div>
-                            </b-col>
-                            <b-col>
-                                We
-                                <div class="date">07 oct</div>
-                            </b-col>
-                            <b-col>
-                                Th
-                                <div class="date">08 oct</div>
-                            </b-col>
-                            <b-col>
-                                Fr
-                                <div class="date">09 oct</div>
+                            <b-col
+                                v-for="date in currentWeek"
+                                :key="date.weekDay"
+                            >
+                                {{date.weekDay}}
+                                <div class="date">{{date.monthDay}} {{date.month}}</div>
                             </b-col>
                         </b-row>
                     </b-container>
@@ -44,11 +31,13 @@
                 <b-col cols="2"></b-col>
             </b-row>
             <project-row
-                v-for="project in currentWeek.projects"
+                v-for="project in projectsRows"
                 :key="project.id"
                 :title="project.title"
                 :description="project.description"
                 :weekInputs="project.hours"
+                :customers="customers"
+                :projects="projects"
                 @on-remove="removeRow(project.id)"
                 @on-update="onUpdate($event)"
             ></project-row>
@@ -81,79 +70,77 @@
 
 <script lang="ts">
 import Vue from 'vue'
-import { addDays, getMonth, getYear, startOfISOWeek, format } from 'date-fns';
+import { mapGetters } from "vuex";
 
 export default Vue.extend({
     computed: {
-        currentWeekLabel: function () {
-            const startDate = startOfISOWeek(new Date(this.currentWeek.startDate));
-            const endDate = addDays(startDate, 0);
-            let label: string = format(startDate, 'dd');
-            if (getMonth(startDate) !== getMonth(endDate)) {
-                label += ` ${format(startDate, 'MMM')}`;
-
-                if (getYear(startDate) !== getYear(endDate)) {
-                    label += ` ${format(startDate, 'yyyy')}`;
-                }
-            }
-            label += ` - ${format(endDate, 'dd MMM yyyy')}`;
-            return label;
-        }
+        ...mapGetters({
+            user: 'auth/getUser',
+            customers: 'customers/getCustomersArray',
+            projects: 'customers/getProjects',
+            weekLabel: 'week-dates/currentWeekLabel',
+            currentWeek: 'week-dates/currentWeek'
+        }),
+    },
+    created() {
+      this.$store.dispatch('customers/getCustomers');
     },
     data() {
       return {
-        currentWeek: {
-            id: 1,
-            startDate: 1605090189379,
-            projects: [
-                {
-                    id: 1,
-                    title: 'Rabobank',
-                    description: 'Afdeling hypotheken',
-                    hours: {
-                        mo: 1,
-                        tu: 2,
-                        we: 3,
-                        th: 4,
-                        fr: 5
-                    }
-                },
-                {
-                    id: 2,
-                    title: 'Rabobank',
-                    description: 'Afdeling verzekeringen',
-                    hours: {
-                        mo: 5,
-                        tu: 2,
-                        we: 10,
-                        th: 4,
-                        fr: 5
-                    }
-                },
-                {
-                    id: 3,
-                    title: 'Postcode loterij',
-                    description: 'Algemeen'
-                },
-            ]
-        },
+        projectsRows: [
+            {
+                id: 1,
+                title: 'Rabobank',
+                description: 'Afdeling hypotheken',
+                hours: {
+                    mo: 1,
+                    tu: 2,
+                    we: 3,
+                    th: 4,
+                    fr: 5
+                }
+            },
+            {
+                id: 2,
+                title: 'Rabobank',
+                description: 'Afdeling verzekeringen',
+                hours: {
+                    mo: 5,
+                    tu: 2,
+                    we: 10,
+                    th: 4,
+                    fr: 5
+                }
+            },
+            {
+                id: 3,
+                title: 'Postcode loterij',
+                description: 'Algemeen'
+            },
+        ]
       }
     },
     methods: {
         addRow: function() {
-            this.currentWeek.projects.push({
+            this.projectsRows.push({
                 id: + new Date(),
                 title: 'Nieuw Project',
-                description: 'Een beschrijving van een nieuw aangemaakt project'
+                description: 'Een beschrijving van een nieuw aangemaakt project',
             });
             this.$store.dispatch('hours/getUsers');
         },
         removeRow: function(projectId: number) {
-            this.currentWeek.projects = this.currentWeek.projects.filter((p) => p.id !== projectId);
+           this.projectsRows = this.projectsRows.filter((p) => p.id !== projectId);
         },
         onUpdate: function(event: any) {
             console.log('Onupdate', event);
         },
+        prevWeek: function() {
+            this.$store.commit('week-dates/prevWeek');
+        },
+        nextWeek: function() {
+            this.$store.commit('week-dates/nextWeek');
+        }
     }
 })
 </script>
