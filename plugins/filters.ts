7528703d6cbc7dcfc9e88122b9e4fb839ee82490
Markus Wind <markuswind@gmail.com>
2021-03-04 12:42:11 +0100

    feat: use composition api in middleware and plugins

--- plugins/filters.ts
+++ plugins/filters.ts
@@ -1,11 +1,11 @@
 import Vue from "vue";
 import { format } from "date-fns";
 
-Vue.filter("formatDate", (value, formatString) => {
-  if (!value) {
-    return "";
-  }
+Vue.filter("formatDate", (value: string, formatString: string) => {
+  if (!value) return "";
+
   formatString = formatString || "";
   const date = new Date(value);
+
   return format(date, formatString);
 });
