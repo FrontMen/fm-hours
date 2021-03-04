09c0cc1eb8ebf74f7231d0ef8a236e5832017d84
Markus Wind <markuswind@gmail.com>
2021-03-04 12:41:49 +0100

    feat: use composition api in components

--- components/last-saved.vue
+++ components/last-saved.vue
@@ -1,50 +1,46 @@
 <template>
   <div>
-    <template v-if="lastSavedDate"> Last saved {{ lastSavedLabel }} </template>
+    <template v-if="lastSavedDate">
+      Last saved {{ lastSavedLabel }}
+    </template>
   </div>
 </template>
 
-<script>
+<script lang="ts">
+import {
+  computed,
+  defineComponent,
+  onBeforeUnmount,
+  ref,
+  useStore,
+  watch,
+} from "@nuxtjs/composition-api";
 import { formatDistanceToNow } from "date-fns";
-import { mapGetters } from "vuex";
-export default {
-  data() {
+
+export default defineComponent({
+  setup() {
+    // FIXME: would be nice it can access holidays store directly
+    const store = useStore<RootStoreState>();
+    // @ts-ignore FIXME: user state is not defined yet
+    const lastSavedDate = computed(() => store.state.user.lastSaved);
+    const lastSavedLabel = ref('')
+
+    const updateLastSavedLabel = () => {
+      lastSavedLabel.value =  formatDistanceToNow(lastSavedDate.value || 0);
+    };
+
+    let intervalHandle: NodeJS.Timeout;
+    const setIntervalHandle = () => {
+      intervalHandle = setInterval(updateLastSavedLabel, 1000);
+    };
+
+    watch(lastSavedDate, setIntervalHandle, { immediate: true });
+    onBeforeUnmount(() => clearInterval(intervalHandle));
+
     return {
-      interval: undefined,
-      lastSavedLabel: undefined,
+      lastSavedDate,
+      lastSavedLabel,
     };
   },
-  computed: {
-    ...mapGetters({
-      lastSavedDate: "user/getLastSavedDate",
-    }),
-  },
-  watch: {
-    lastSavedDate: {
-      immediate: true,
-      handler(val) {
-        if (val) {
-          this.lastSavedLabel = this.updateLastSavedLabel(this.lastSavedDate);
-          this.setInterval();
-        }
-      },
-    },
-  },
-  destroyed() {
-    clearInterval(this.interval);
-  },
-  methods: {
-    setInterval() {
-      if (this.interval) {
-        clearInterval(this.interval);
-      }
-      this.interval = setInterval(() => {
-        this.lastSavedLabel = this.updateLastSavedLabel(this.lastSavedDate);
-      }, 10000);
-    },
-    updateLastSavedLabel(date) {
-      return formatDistanceToNow(date, { addSuffix: true });
-    },
-  },
-};
+});
 </script>
