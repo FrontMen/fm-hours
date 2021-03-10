c7a3d955a1758a27d3cd7a306ee9df4957ecda01
Markus Wind <markuswind@gmail.com>
2021-03-10 14:18:14 +0100

    feat: save records to firestore

--- store/records/actions.ts
+++ store/records/actions.ts
@@ -1,54 +1,77 @@
+/* eslint-disable camelcase */
 import { ActionTree } from "vuex";
 
 const actions: ActionTree<RecordsStoreState, RootStoreState> = {
-  getRecords(
-    { commit, rootState },
-    payload: { startDate: Date }
-  ) {
-    commit("setLoading", { isLoading: true })
+  async getRecords({ commit, rootState }, payload: { startDate: Date }) {
+    commit("setLoading", { isLoading: true });
 
     const userId = rootState.user.user?.id;
-    // eslint-disable-next-line no-console
-    console.log("TODO: get records from firestore with parameters", {
-      payload,
-      userId,
-    });
+    const ref = this.$fire.firestore.collection("records").doc(userId);
+    const doc = await ref.get();
 
-    const result = {
-      time_records: [],
-      travel_records: [{
-        date: '03-10-2021',
-        kilometers: 8,
-        status: 'NEW'
-      }],
-    };
+    if (doc.exists) {
+      const data = doc.data();
 
-    commit("setLoading", { isLoading: false })
-    commit("setRecords", {
-      timeRecords: result.time_records,
-      travelRecords: result.travel_records,
-      startDate: payload.startDate,
-    });
+      commit("setRecords", {
+        timeRecords: data?.time_records || [],
+        travelRecords: data?.travel_records || [],
+        startDate: payload.startDate,
+      });
+    } else {
+      commit("setRecords", {
+        timeRecords: [],
+        travelRecords: [],
+        startDate: payload.startDate
+      })
+    }
+
+    commit("setLoading", { isLoading: false });
   },
 
-  saveRecords(
+  saveTimesheet(
     { commit, rootState },
-    payload: { timeRecords: TimeRecord[]; travelRecords: TravelRecord[] }
+    payload: { week: WeekDate[]; timesheet: WeeklyTimesheet }
   ) {
-    commit("setSaving", { isSaving: true })
+    commit("setSaving", { isSaving: true });
 
-    const userId = rootState.user.user?.id;
-    // eslint-disable-next-line no-console
-    console.log("TODO: save records from firestore with parameters", {
-      payload,
-      userId,
+    const timeRecordsToSave: TimeRecord[] = [];
+    const travelRecordsToSave: TravelRecord[] = [];
+
+    payload.timesheet.projects.forEach((project) => {
+      project.values.forEach((value, index) => {
+        if (value === 0) return;
+
+        timeRecordsToSave.push({
+          date: payload.week[index].date,
+          customer: project.customer,
+          hours: value,
+          status: "new" as RecordStatus,
+        });
+      });
     });
 
-    commit("setSaving", { isSaving: false })
-    commit("setRecords", {
-      timeRecords: payload.timeRecords,
-      travelRecords: payload.travelRecords,
-    })
+    payload.timesheet.travelProject?.values.forEach((value, index) => {
+      if (value === 0) return;
+
+      travelRecordsToSave.push({
+        date: payload.week[index].date,
+        kilometers: value,
+        status: "new" as RecordStatus,
+      });
+    });
+
+    const userId = rootState.user.user?.id;
+    const recordsRef = this.$fire.firestore.collection("records").doc(userId);
+
+    if (timeRecordsToSave.length > 0) {
+      recordsRef.set({ time_records: timeRecordsToSave }, { merge: true });
+    }
+
+    if (travelRecordsToSave.length > 0) {
+      recordsRef.set({ travel_records: travelRecordsToSave }, { merge: true });
+    }
+
+    commit("setSaving", { isSaving: false });
   },
 };
 
