4bc4c68232ff54a0f292bbed15b7a352925cf6dc
Markus Wind <markuswind@gmail.com>
2021-03-12 16:36:34 +0100

    refactor: moved holidays firestore logic to service

--- plugins/services.ts
+++ plugins/services.ts
@@ -1,11 +1,13 @@
-import { defineNuxtPlugin } from '@nuxtjs/composition-api'
+import { defineNuxtPlugin } from "@nuxtjs/composition-api";
 
-import RecordsService from '~/services/records-service'
-import UsersService from '~/services/users-service'
-import WorkSchemeService from '~/services/work-scheme-service'
+import HolidaysService from "~/services/holidays-service";
+import RecordsService from "~/services/records-service";
+import UsersService from "~/services/users-service";
+import WorkSchemeService from "~/services/work-scheme-service";
 
 export default defineNuxtPlugin(({ $fire }, inject) => {
-  inject('recordsService', new RecordsService($fire))
-  inject('usersService', new UsersService($fire))
-  inject('workSchemeService', new WorkSchemeService())
-})
+  inject("holidaysService", new HolidaysService($fire));
+  inject("recordsService", new RecordsService($fire));
+  inject("usersService", new UsersService($fire));
+  inject("workSchemeService", new WorkSchemeService());
+});
--- store/holidays/mutations.ts
+++ store/holidays/mutations.ts
@@ -4,10 +4,6 @@ const mutations: MutationTree<HolidaysStoreState> = {
   setHolidays(state, payload: { holidays: string[] }) {
     state.holidays = payload.holidays;
   },
-
-  setHolidayDocId(state, payload: { docId: string }) {
-    state.docId = payload.docId;
-  },
 };
 
 export default mutations;
