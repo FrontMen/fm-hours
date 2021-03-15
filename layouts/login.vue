658e471022f1d5eda100021e0448e42069f59ef0
Markus Wind <markuswind@gmail.com>
2021-03-15 09:50:00 +0100

    chore: remove unused code from login layout

--- layouts/login.vue
+++ layouts/login.vue
@@ -1,37 +1,8 @@
 <template>
-  <div class="layout-wrapper">
-    <div />
-    <Nuxt />
-  </div>
+  <Nuxt />
 </template>
 
 <script lang="ts">
-import { computed, defineComponent, useStore, useContext } from '@nuxtjs/composition-api'
-
-export default defineComponent({
-  setup() {
-    // FIXME: would be nice it can access user store directly
-    const store = useStore<RootStoreState>();
-    // @ts-ignore FIXME: user state is not defined yet
-    const user = computed(() => store.user.user);
-
-    const logout = () => {
-      useContext().$fire.auth.signOut()
-    }
-
-    return {
-      user,
-      logout,
-    }
-  }
-})
+import { defineComponent } from "@nuxtjs/composition-api";
+export default defineComponent({});
 </script>
-
-<style lang="scss">
-.top-bar {
-  img {
-    width: 50px;
-    max-height: 100%;
-  }
-}
-</style>
