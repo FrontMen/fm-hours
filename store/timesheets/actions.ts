75c8a16b41c0f96487943dbd774a031ab8995bb3
Markus Wind <markuswind@gmail.com>
2021-03-16 09:46:17 +0100

    feat: added base of timesheet detail page

--- store/timesheets/actions.ts
+++ store/timesheets/actions.ts
@@ -2,26 +2,38 @@
 import { ActionTree } from "vuex";
 import { recordStatus } from "~/helpers/record-status";
 
+const isPendingRecord = (record: TimeRecord | TravelRecord, userId: string) =>
+  record.userId === userId && record.status === recordStatus.PENDING;
+
+const isDeniedRecord = (record: TimeRecord | TravelRecord, userId: string) =>
+  record.userId === userId && record.status === recordStatus.DENIED;
+
 const actions: ActionTree<TimesheetsStoreState, RootStoreState> = {
   async getUserList({ commit }) {
     const timeRecords = await this.app.$timeRecordsService.getPendingOrDeniedRecords();
-    // TODO: check travel records as well?
+    const travelRecords = await this.app.$travelRecordsService.getPendingOrDeniedRecords();
 
     const users = await this.app.$usersService.getUsers();
 
     const timesheetUsers = users.map((user) => {
-      const hasPendingRecords = timeRecords.some(
-        (record) =>
-          record.userId === user.id && record.status === recordStatus.PENDING
+      const pendingTimeRecords = timeRecords.filter((record) =>
+        isPendingRecord(record, user.id)
       );
 
-      const hasDeniedRecords = timeRecords.some(
-        (record) =>
-          record.userId === user.id && record.status === recordStatus.DENIED
+      const pendingTravelRecords = timeRecords.filter((record) =>
+        isPendingRecord(record, user.id)
       );
+
+      const hasPendingRecords =
+        pendingTimeRecords.length > 0 || pendingTravelRecords.length > 0;
+      const hasDeniedRecords =
+        timeRecords.some((record) => isDeniedRecord(record, user.id)) ||
+        travelRecords.some((record) => isDeniedRecord(record, user.id));
 
       return {
         ...user,
+        pendingTimeRecords,
+        pendingTravelRecords,
         status: hasPendingRecords
           ? recordStatus.PENDING
           : hasDeniedRecords
--- types/timesheets.d.ts
+++ types/timesheets.d.ts
@@ -1,5 +1,11 @@
 interface TimesheetUser extends User {
   status: RecordStatus;
+  pendingTimeRecords: TimeRecord[];
+  pendingTravelRecords: TravelRecord[];
+}
+
+interface TimesheetPendingWeek {
+  week: WeekDate[];
 }
 
 interface TimesheetsStoreState {
