1f95985e245802b3531213c3447ec6c8cc142f16
Markus Wind <markuswind@gmail.com>
2021-03-16 10:50:36 +0100

    feat: added getters for pending records data

--- pages/timesheets/_user_id.vue
+++ pages/timesheets/_user_id.vue
@@ -7,19 +7,51 @@
 </template>
 
 <script lang="ts">
-import { defineComponent, useStore, useRouter } from "@nuxtjs/composition-api";
+import {
+  computed,
+  defineComponent,
+  ref,
+  useStore,
+  useRouter,
+  watch,
+} from "@nuxtjs/composition-api";
 import { recordStatus } from "~/helpers/record-status";
 
 export default defineComponent({
   setup() {
     const store = useStore<RootStoreState>();
     const router = useRouter();
-    const userId = router.currentRoute.params.user_id;
 
-    // TODO: have pending timeRecords
-    // TODO: have pending travelRecords
+    const userId = router.currentRoute.params.user_id;
+    const users = computed(() => store.state.timesheets.users);
+
+    const pendingTimeRecords = ref<TimeRecord[]>();
+    const pendingTravelRecords = ref<TravelRecord[]>();
+    const pendingWeeks = ref<TimesheetPendingWeek[]>();
+
+    if (users.value.length === 0) {
+      store.dispatch("timesheets/getUserList");
+    }
+
+    watch(
+      () => users.value,
+      () => {
+        pendingTimeRecords.value = store.getters[
+          "timesheets/getUserPendingTimeRecords"
+        ](userId);
+
+        pendingTravelRecords.value = store.getters[
+          "timesheets/getUserPendingTravelRecords"
+        ](userId);
+
+        pendingWeeks.value = store.getters["timesheets/getPendingWeeks"](
+          userId
+        );
+      },
+      { deep: true, immediate: true }
+    );
+
     // TODO: have work scheme
-    // TODO: have array of weeks to create array of timesheets
 
     const saveTimesheet = (
       week: WeekDate[],
