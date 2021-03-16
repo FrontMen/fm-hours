63e3704436a234d6076f080c475e62c6e3eed2ed
Markus Wind <markuswind@gmail.com>
2021-03-16 10:06:29 +0100

    refactor: extracted timesheet user row into component

---
+++ components/timesheets/timesheet-user-row.vue
@@ -0,0 +1,58 @@
+<template>
+  <b-row
+    class="timesheet-user-row app-table__row py-3"
+    no-gutters
+    @click="handleClick"
+  >
+    <b-col cols="0" class="mr-4">
+      <b-avatar :src="user.picture" />
+    </b-col>
+
+    <b-col>
+      <div class="font-weight-bold">
+        {{ user.name }}
+      </div>
+
+      <div v-if="user.status === recordStatus.NEW" class="text-success">
+        Nothing to approve
+      </div>
+
+      <div v-if="user.status === recordStatus.PENDING" class="text-warning">
+        Waiting on approval
+      </div>
+
+      <div v-if="user.status === recordStatus.DENIED" class="text-danger">
+        Has denied records
+      </div>
+    </b-col>
+  </b-row>
+</template>
+
+<script lang="ts">
+import { defineComponent, PropType } from "@nuxtjs/composition-api";
+import { recordStatus } from "~/helpers/record-status";
+
+export default defineComponent({
+  emits: ["click"],
+  props: {
+    user: {
+      type: Object as PropType<TimesheetUser>,
+      required: true,
+    },
+  },
+  setup(_, { emit }) {
+    const handleClick = () => emit("click");
+
+    return {
+      handleClick,
+      recordStatus,
+    };
+  },
+});
+</script>
+
+<style lang="scss">
+.timesheet-user-row {
+  cursor: pointer;
+}
+</style>
