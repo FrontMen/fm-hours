b4e29b88967eb1b76e76f52e1a8cae8fd734d95f
Italo <italo.brtx@gmail.com>
2021-05-18 17:55:53 -0300

    feat(timesheets): timezone support

--- helpers/dates.ts
+++ helpers/dates.ts
@@ -1,6 +1,5 @@
 import {
   format,
-  formatISO,
   isToday,
   isWeekend,
   isSameDay,
@@ -75,12 +74,6 @@ export function getWeekRange(beginDate: string) {
   return { start, end };
 }
 
-// Get ISO date string with the format "YYYY-MM-DD" on the GMT time, so we can
-// easily check for date equality wihout worrying abou hours or timezone differences
-export function getISODay(date: number | Date) {
-  return formatISO(date).slice(0, 10);
-}
-
 function buildWeekSpan(startDate: Date) {
   const start = startOfISOWeek(startDate);
   const end = addDays(start, 6);
@@ -89,12 +82,12 @@ function buildWeekSpan(startDate: Date) {
     start: {
       date: start.getTime(),
       formatedDate: format(start, "dd/MMM"),
-      ISO: getISODay(start),
+      ISO: formatDate(start),
     },
     end: {
       date: end.getTime(),
       formatedDate: format(end, "dd/MMM"),
-      ISO: getISODay(end),
+      ISO: formatDate(end),
     },
   };
 }
