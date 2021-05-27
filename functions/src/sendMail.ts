a13d0c1920a559688ab35af27a3f1d2c574b90c6
Italo <italo.brtx@gmail.com>
2021-05-27 10:45:36 -0300

    refactor(functions): change to OAuth 2.0

--- functions/src/sendMail.ts
+++ functions/src/sendMail.ts
@@ -1,32 +1,73 @@
 /* eslint-disable @typescript-eslint/no-var-requires */
 import * as functions from "firebase-functions";
-import { createTransport, SendMailOptions } from "nodemailer";
+import { createTransport, SendMailOptions, TransportOptions } from "nodemailer";
+import { google } from "googleapis";
 const cors = require("cors")({ origin: true });
+const OAuth2 = google.auth.OAuth2;
 
 export const sendMail = functions.https.onRequest((req, res) => {
   return cors(req, res, () => {
-    const transporter = createTransport({
-      service: "gmail",
-      auth: {
-        user: functions.config().mail.user,
-        pass: functions.config().mail.pass,
-      },
-    });
+    const createTransporter = async () => {
+      const oauth2Client = new OAuth2(
+        functions.config().mail.clientid,
+        functions.config().mail.clientsecret,
+        "https://developers.google.com/oauthplayground"
+      );
+
+      oauth2Client.setCredentials({
+        refresh_token: functions.config().mail.refreshtoken,
+      });
+
+      const accessToken = await new Promise((resolve, reject) => {
+        oauth2Client.getAccessToken((err, token) => {
+          if (err) {
+            reject(new Error("Failed to create access token"));
+          }
+          resolve(token);
+        });
+      });
+
+      const transporter = createTransport({
+        service: "gmail",
+        auth: {
+          type: "OAuth2",
+          user: functions.config().mail.user,
+          accessToken,
+          clientId: functions.config().mail.clientid,
+          clientSecret: functions.config().mail.clientsecret,
+          refreshToken: functions.config().mail.refreshtoken,
+        },
+      } as TransportOptions);
+
+      return transporter;
+    };
 
     const { to, subject, html } = req.body;
 
     const mailOptions = {
-      from: `Frontmen <${functions.config().mail.user}>"`,
+      from: `Uren <${functions.config().mail.user}>"`,
       to,
       subject,
       html,
     } as SendMailOptions;
 
-    return transporter.sendMail(mailOptions, (error) => {
-      if (error) {
-        return res.status(500).send(error.toString());
-      }
-      return res.send("E-mail sent successfully!");
-    });
+    const sendEmail = async (emailOptions: SendMailOptions) => {
+      const emailTransporter = await createTransporter();
+
+      return emailTransporter.sendMail(emailOptions, (error) => {
+        if (error) {
+          functions.logger.log("error", error);
+          return res.status(500).send(error.toString());
+        }
+        return res.send("E-mail sent successfully!");
+      });
+    };
+
+    try {
+      return sendEmail(mailOptions);
+    } catch (err) {
+      functions.logger.log("error", err);
+      return res.status(500).send(err.toString());
+    }
   });
 });
