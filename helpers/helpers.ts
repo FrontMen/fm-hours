126e0bdbf48af493afb48b6f38cac0938d21e1ba
Italo <italo.brtx@gmail.com>
2021-06-11 07:31:42 -0300

    feat(helpers): add sleep function

--- helpers/helpers.ts
+++ helpers/helpers.ts
@@ -2,3 +2,7 @@ export const uniqueByKey = <T>(array: T[], key: string): T[] => [
   // @ts-ignore
   ...new Map(array.map((item) => [item[key], item])).values(),
 ];
+
+export const sleep = (milliseconds: number) => {
+  return new Promise((resolve) => setTimeout(resolve, milliseconds));
+};
