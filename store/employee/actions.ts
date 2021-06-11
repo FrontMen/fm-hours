b17e7392de3224bbb1601fa921003e390ee8f340
Italo <italo.brtx@gmail.com>
2021-06-11 07:44:11 -0300

    fix(employee): add retry logic to first time login

--- store/employee/actions.ts
+++ store/employee/actions.ts
@@ -1,42 +1,55 @@
 /* eslint-disable camelcase */
 import { ActionTree } from "vuex";
+
 import EmployeesService from "~/services/employees-service";
+import { sleep } from "~/helpers/helpers";
 
 const actions: ActionTree<EmployeeStoreState, RootStoreState> = {
-  async login() {
-    const provider = new this.$fireModule.auth.GoogleAuthProvider();
-    await this.$fire.auth.signInWithPopup(provider);
+  async login({ commit }) {
+    try {
+      commit("setLoading", true);
+      commit("setErrorMessage", "");
+      const provider = new this.$fireModule.auth.GoogleAuthProvider();
+      await this.$fire.auth.signInWithPopup(provider);
+    } catch (err) {
+      commit("setLoading", false);
+      commit(
+        "setErrorMessage",
+        "An unexpected error happened while trying to log in"
+      );
+    }
   },
 
   logout({ commit }) {
     this.$fire.auth.signOut();
     this.app.router?.push("/");
 
+    commit("setLoading", false);
     commit("resetEmployee");
   },
 
   async onAuthStateChanged(
-    { commit },
+    { commit, dispatch },
     payload: { authUser?: any; claims?: any }
   ) {
     if (!payload.authUser) return;
     const employeesService = new EmployeesService(this.$fire);
-    const employee = await employeesService.getEmployee(payload.claims.user_id);
+    let employee = await employeesService.getEmployee(payload.claims.user_id);
     const isAdmin = await employeesService.isAdmin(payload.claims.email);
+
+    if (!employee) {
+      await sleep(3000);
+      employee = await employeesService.getEmployee(payload.claims.user_id);
+    }
 
     if (employee) {
       commit("setEmployee", { employee, isAdmin });
     } else {
-      const newEmployee = {
-        id: payload.claims.user_id,
-        name: payload.claims.name,
-        picture: payload.claims.picture,
-        email: payload.claims.email,
-        projects: [],
-        travelAllowance: false,
-        endDate: null,
-      };
-      commit("setEmployee", { employee: newEmployee, isAdmin });
+      commit(
+        "setErrorMessage",
+        "An unexpected error happened while trying to log in"
+      );
+      dispatch("employee/logout");
     }
   },
 };
