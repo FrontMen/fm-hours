ac7eadd87e26f7d7cc2d1a3db017ee58be252719
Italo <italo.brtx@gmail.com>
2021-06-16 09:39:05 -0300

    feat(helpers): add string helpers

--- helpers/helpers.ts
+++ helpers/helpers.ts
@@ -6,3 +6,13 @@ export const uniqueByKey = <T>(array: T[], key: string): T[] => [
 export const sleep = (milliseconds: number) => {
   return new Promise((resolve) => setTimeout(resolve, milliseconds));
 };
+
+// Remove accents from strings
+export const normalizeText = (text: string) =>
+  text.normalize("NFD").replace(/[\u0300-\u036F]/g, "");
+
+// Return if a target string is or isn't a match for a seach (query) value
+export const queryOnString = (target: string, query: string) =>
+  normalizeText(target)
+    .toUpperCase()
+    .includes(normalizeText(query).toUpperCase());
