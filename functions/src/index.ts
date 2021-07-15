ca9c16fce1c42c6f3fad14a556bd69fa65bda20e
Ítalo Teixeira <italo.brtx@gmail.com>
2021-07-15 17:59:34 +0200

    refactor(functions): remove create employee

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,9 +1,7 @@
 import * as admin from "firebase-admin";
 
-import { createEmployee } from "./createEmployee";
 import { sendMail } from "./sendMail";
 
 admin.initializeApp();
 
-exports.createEmployee = createEmployee;
 exports.sendMail = sendMail;
