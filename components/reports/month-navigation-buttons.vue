0603ac218ba1037c879bae7dc4f0869b26d224b4
Vlad Pintea <vlad.pintea@frontmen.nl>
2021-07-19 14:22:26 +0200

    Refactor - syntax fix

--- components/reports/month-navigation-buttons.vue
+++ components/reports/month-navigation-buttons.vue
@@ -19,7 +19,7 @@
       <h2 class="navigation-buttons__week-label">
         {{ monthLabel }}
       </h2>
-      <b-button v-if="monthDifference !== 0" @click="handleCurrentClick()">
+      <b-button v-if="monthDifference !== 0" @click="handleCurrentClick">
         <b-icon icon="calendar2-date" />
         <span class="ml-2 d-none d-sm-inline">To current month</span>
       </b-button>
