91841eb9292f520c7d8d783e9c8e91b086b715ef
Mehmet Ali IZCI <mmehmetaliizci@hotmail.com>
2021-07-26 15:39:04 +0200

    review: fill ids of null instead of uuid

--- helpers/timesheet.ts
+++ helpers/timesheet.ts
@@ -105,7 +105,7 @@ export const createLeaveProject = (
       isBillable: false,
       isDefault: false,
     },
-    ids: hours.map(_ => uuidv4()),
+    ids: new Array(hours.length).fill(null),
     values: hours,
     isExternal: false,
   }: null;
@@ -113,12 +113,6 @@ export const createLeaveProject = (
 
 const findLeaveHoursByDate = (leaveDay: WeekDate, workScheme: WorkScheme[]): number =>
   workScheme.find((workSchemeDate: WorkScheme) => workSchemeDate.date === leaveDay.date)?.absenceHours ?? 0
-
-function uuidv4() {
-  return '00-0-4-1-000'.replace(/[^-]/g,
-    (s:any) => ((Math.random() + ~~s) * 0x10000 >> s).toString(16).padStart(4, '0')
-  );
-}
 
 const findRecordByDate = (
   weekDay: WeekDate,
