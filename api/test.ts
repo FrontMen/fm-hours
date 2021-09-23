593a6d7dc6c3f46d0605101369bf9701e2131c2c
Raul Melo <melo.raulf@gmail.com>
2021-09-23 11:34:21 +0200

    chore: add axios

--- api/test.ts
+++ api/test.ts
@@ -1,11 +1,15 @@
 import {VercelRequest, VercelResponse} from '@vercel/node';
+import axios from 'axios';
 
-export default function (req: VercelRequest, res: VercelResponse) {
+export default async function (req: VercelRequest, res: VercelResponse) {
+  const {data} = await axios.get('https://pokeapi.co/api/v2/pokemon');
+
   res.json({
     body: {
       ...req.body,
       hello: 'world',
       test: 'test',
+      data,
     },
     query: req.query,
     cookies: req.cookies,
