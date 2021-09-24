692359e67ae1a8df8c5cb2e08e41d8fce5fddcb2
Raul Melo <melo.raulf@gmail.com>
2021-09-24 11:19:01 +0200

    refactor: remove unnecessary mutation

--- store/auth/mutations.ts
+++ store/auth/mutations.ts
@@ -1,27 +1,6 @@
 import type {MutationTree} from 'vuex';
 
 const mutations: MutationTree<AuthStoreState> = {
-  ON_AUTH_STATE_CHANGED_MUTATION: (state, {authUser}) => {
-    if (!authUser) {
-      state.user = undefined;
-      return;
-    }
-
-    const {uid, email, emailVerified, displayName, photoURL} = authUser;
-
-    const user = {
-      uid,
-      displayName,
-      email,
-      emailVerified,
-      photoURL: photoURL || null,
-      // TODO: investigate why we're doing this logic twice
-      samlToken: authUser.b.b.g,
-    };
-
-    state.user = user;
-  },
-
   setUser: (state, user: User) => {
     state.user = user;
   },
