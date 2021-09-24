5b2c55bfc2275904a63a0504215c4731e0c72b7c
Raul Melo <melo.raulf@gmail.com>
2021-09-24 16:52:43 +0200

    refactor: enhance auth actions

--- store/auth/actions.ts
+++ store/auth/actions.ts
@@ -2,7 +2,7 @@ import {ActionTree} from 'vuex/types/index';
 import {extractUserFromAuthUser} from '~/helpers/auth';
 
 const actions: ActionTree<AuthStoreState, RootStoreState> = {
-  async login({commit}) {
+  async login({dispatch}) {
     try {
       const provider = new this.$fireModule.auth.SAMLAuthProvider(
         'saml.intracto'
@@ -10,13 +10,12 @@ const actions: ActionTree<AuthStoreState, RootStoreState> = {
       const {user} = await this.$fire.auth.signInWithPopup(provider);
 
       if (user) {
-        const userToCommit = await extractUserFromAuthUser(user);
-
-        commit('setUser', userToCommit);
-
+        await dispatch('_saveUser', user);
         return true;
       }
     } catch (error: any) {
+      // eslint-disable-next-line no-console
+      console.error('Something went wrong while Login');
       throw new Error(error);
     }
   },
@@ -28,17 +27,46 @@ const actions: ActionTree<AuthStoreState, RootStoreState> = {
     return true;
   },
 
+  /**
+   * This actions is only called via Nuxt Firebase.
+   */
   async onAuthStateChangedAction({commit, dispatch}, {authUser}) {
     if (!authUser) {
       commit('resetUser');
       return;
     }
 
-    const user = await extractUserFromAuthUser(authUser);
-
-    commit('setUser', user);
+    await dispatch('_saveUser', authUser);
 
     if (!process.server) dispatch('employee/getEmployee', {}, {root: true});
+  },
+
+  /**
+   * Internals
+   */
+  async _saveUser({commit}, authUser) {
+    // 1. extract user data from auth
+    const user = await extractUserFromAuthUser(authUser);
+
+    // 2. check if it has userId
+    if (!user.bridgeUid) {
+      // TODO: maybe revert auth-service deletion and move this call there
+      // 3. if not, calls /api/user/me with samlToken to retried bridgeUid
+      const {data} = await this.$axios.get<{bridgeUid: number}>(
+        '/api/user/me',
+        {
+          headers: {Authorization: user.samlToken},
+        }
+      );
+
+      // 3.1 assigns bridgeUid to the user who's gonna be saved
+      user.bridgeUid = data.bridgeUid;
+
+      // 3.1 saves that bridgeUid to user (employee) with Firebase
+      // TODO: implement that
+    }
+
+    commit('setUser', user);
   },
 };
 
