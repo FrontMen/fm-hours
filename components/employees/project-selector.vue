b0110e254c5789843c4601d4e4f88f17b398025e
Sander de Jong <sander@frontmen.nl>
2022-03-03 21:00:34 +0100

    feat: allow selecting/removing contract related to projects

--- components/employees/project-selector.vue
+++ components/employees/project-selector.vue
@@ -2,20 +2,18 @@
 en:
   manageProjects: "Manage Projects"
   customerSearchPlaceholder: "Click or search for a customer here"
-  addContractModalTitle: "Add contract"
-  addContractModalInputPlaceholder: "Search term"
+  removeContract: "Remove contract"
 nl:
-  manageProjects: "Projecten bewerkern"
+  manageProjects: "Projecten bewerken"
   customerSearchPlaceholder: "Klik of zoek naar een klant"
-  addContractModalTitle: "Voeg contract toe"
-  addContractModalInputPlaceholder: "Zoek term"
+  removeContract: "Verwijder contract"
 </i18n>
 
 <template>
   <section>
     <h6 class="mb-3">{{ $t('manageProjects') }}</h6>
     <multiselect
-      :value="selectedCustomers"
+      :value="selectedProjects"
       track-by="id"
       label="label"
       class="mb-3"
@@ -24,7 +22,7 @@ nl:
       :multiple="true"
       :taggable="false"
       :placeholder="$t('customerSearchPlaceholder')"
-      @input="$emit('update-selected-customers', $event)"
+      @input="$emit('update-selected-projects', $event)"
     >
       <template slot="selection" slot-scope="{values}">
         <span v-if="values.length" class="multiselect__single">
@@ -34,17 +32,29 @@ nl:
     </multiselect>
 
     <b-table
-      :items="selectedCustomers"
-      :fields="['name', 'debtor', {key: 'contract', class: 'text-center'}, {key: 'delete', class: 'text-center'}]"
+      :items="selectedProjects"
+      :fields="fields"
       class="rounded"
       small
       striped
       table-variant="light"
     >
-      <template #cell(contract)>
-        <b-button v-b-modal.add-contract size="sm" variant="success">
-          <b-icon-plus />
-        </b-button>
+      <template #cell(contract)="data">
+        <div v-if="data.item.contract">
+          <b-button size="sm" @click="handleOpenContract(data.item)">
+            <b-icon-eye />
+          </b-button>
+        </div>
+        <div v-else>
+          <b-button
+            v-b-modal.select-contract
+            size="sm"
+            variant="success"
+            @click="selectedCustomer = data.item"
+          >
+            <b-icon-plus />
+          </b-button>
+        </div>
       </template>
       <template #cell(delete)="row">
         <b-button
@@ -58,40 +68,37 @@ nl:
       </template>
     </b-table>
 
-    <b-modal id="add-contract" size="xl" scrollable :title="$t('addContractModalTitle')">
-      <b-form-input
-        v-model="searchQuery"
-        autofocus
-        type="text"
-        :placeholder="$t('addContractModalInputPlaceholder')"
-        :trim="true"
-        :debounce="300"
-        @update="callSearch"
-      />
-      <b-table
-        :items="foundContracts"
-        :fields="['name', 'project_name', 'project_billingentity_name']"
-        class="rounded"
-        small
-        striped
-        table-variant="light"
-      ></b-table>
+    <contract-selector id="select-contract" @selected="handleContractSelected"></contract-selector>
+
+    <b-modal ref="viewContractModal" ok-only>
+      <b-button size="sm" variant="danger" @click="handleContractDelete(selectedProject)">
+        <b-icon-trash-fill />
+        {{ $t('removeContract') }}
+      </b-button>
+      <pre v-if="selectedProject">
+        {{ selectedProject.contract }}
+      </pre>
     </b-modal>
   </section>
 </template>
 
 <script lang="ts">
-import {computed, defineComponent, ref, SetupContext, useContext} from "@nuxtjs/composition-api";
+import {computed, defineComponent, PropType, ref, SetupContext} from "@nuxtjs/composition-api";
+
+export interface Project {
+  customer: Customer,
+  contract: Contract
+}
 
 interface ProjectSelectorProps {
-  selectedCustomers: Customer[],
+  selectedProjects: Project[],
   customers: Customer[]
 }
 
 export default defineComponent({
   props: {
-    selectedCustomers: {
-      type: Array,
+    selectedProjects: {
+      type: Array as PropType<Project[]>,
       required: true,
       default: () => []
     },
@@ -101,14 +108,12 @@ export default defineComponent({
       default: () => []
     },
   },
-  emits: ['update-selected-customers'],
+  emits: ['update-selected-projects'],
   setup(props: ProjectSelectorProps, {emit}: SetupContext) {
-    const {app, i18n} = useContext();
-
     const handleProjectDelete = (projectId: string) => {
-      const newList = props.selectedCustomers.filter((customer: Customer) => customer.id !== projectId);
+      const newList = props.selectedProjects.filter(({customer}) => customer.id !== projectId);
 
-      emit('update-selected-customers', newList);
+      emit('update-selected-projects', newList);
     }
 
     const customerOptions = computed(() =>
@@ -120,28 +125,65 @@ export default defineComponent({
         }))
     );
 
-    const searchQuery = ref('');
-    const foundContracts = ref([]);
-    let axiosAbortController: AbortController;
 
-    const callSearch = async (value: string) => {
-      if (axiosAbortController) {
-        axiosAbortController.abort();
-      }
-      axiosAbortController = new AbortController();
+    const selectedProject = ref<Project>();
+    const selectedCustomer = ref();
 
-      try {
-        const {contractList} = await app.$contractsService.getContractByParam(axiosAbortController.signal, {search: value});
-
-        foundContracts.value = contractList.length > 0 ? contractList : [{name: i18n.t('noContractsFound')}];
-      } catch (e) {
-        if (e.name !== 'AbortError') {
-          console.error(e);
+    const handleContractSelected = (contract: Contract) => {
+      const newList = props.selectedProjects.map((record) => {
+        if (record.customer.id === selectedCustomer.value.customer.id) {
+          return {
+            ...record,
+            contract
+          }
         }
-      }
+        return record;
+      });
+
+      selectedCustomer.value = undefined;
+
+      emit('update-selected-projects', newList);
     }
 
-    return {searchQuery, foundContracts, callSearch, handleProjectDelete, customerOptions}
+    const viewContractModal = ref(null);
+    const handleOpenContract = (contract: Contract) => {
+      selectedProject.value = contract;
+      viewContractModal.value.show();
+    }
+
+    const handleContractDelete = (project: Project) => {
+      const newList = props.selectedProjects.map((record) => {
+        if (record.customer.id === project.customer.id) {
+          return {
+            ...record,
+            contract: null
+          }
+        }
+        return record;
+      });
+
+      emit('update-selected-projects', newList);
+      viewContractModal.value.hide();
+    }
+
+    const fields = [
+      {key: 'customer.name', label: 'Customer'},
+      {key: 'customer.debtor', label: 'Debtor'},
+      {key: 'contract', class: 'text-center'},
+      {key: 'delete', class: 'text-center'}
+    ];
+
+    return {
+      selectedProject,
+      selectedCustomer,
+      handleProjectDelete,
+      handleContractSelected,
+      customerOptions,
+      fields,
+      handleOpenContract,
+      viewContractModal,
+      handleContractDelete
+    }
   },
 });
 </script>
