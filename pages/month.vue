9f624163d854515479ff41729a63f063a24ce10c
clickspider <danielfrey101@gmail.com>
2022-05-31 11:08:34 +0200

    feat(insights-for-employees): reverted changes that made in the month route

--- pages/month.vue
+++ pages/month.vue
@@ -9,26 +9,24 @@
 </template>
 
 <script lang="ts">
-import {defineComponent, ref, useContext, useMeta, watch,useRouter} from '@nuxtjs/composition-api';
-import {addMonths, endOfMonth, format, startOfMonth, subMonths} from 'date-fns';
-import { getMonthName } from '~/helpers/helpers';
+import {defineComponent, ref, useContext, useMeta, watch} from '@nuxtjs/composition-api';
+import {addMonths, endOfMonth, startOfMonth, subMonths} from 'date-fns';
+import {getMonthName} from '~/helpers/helpers';
 
 export default defineComponent({
   setup() {
     const {i18n} = useContext();
-    const router = useRouter();
 
-    const monthParam = router.currentRoute.params.month;
-    const selectedMonthNumber = +monthParam -1;
+    const date = new Date();
+
+    const monthStartDate = ref<Date>(startOfMonth(date));
+    const monthEndDate = ref<Date>(endOfMonth(date));
 
     useMeta(() => ({
-      title: `${i18n.t('monthlyReport')} - ${getMonthName(selectedMonthNumber)}` as string,
+      title: `${i18n.t('monthlyReport')} - ${getMonthName(
+        monthStartDate.value.getMonth()
+      )}` as string,
     }));
-
-    const currentYear = new Date().getFullYear();
-    const selectedMonthWithCurrentYear = new Date(currentYear, selectedMonthNumber);
-    const monthStartDate = ref<Date>(startOfMonth(selectedMonthWithCurrentYear));
-    const monthEndDate = ref<Date>(endOfMonth(selectedMonthWithCurrentYear));
 
     watch(
       [monthStartDate],
@@ -40,25 +38,16 @@ export default defineComponent({
       }
     );
 
-    const updateRouterPath = (date: Date) => {
-      router.replace({
-        path: `/month/${format(date, 'MM')}`,
-      });
-    };
-
     const goToPreviousMonth = () => {
       monthStartDate.value = subMonths(monthStartDate.value as Date, 1);
-      updateRouterPath(monthStartDate.value as Date);
     };
 
     const goToNextMonth = () => {
       monthStartDate.value = addMonths(monthStartDate.value as Date, 1);
-      updateRouterPath(monthStartDate.value as Date);
     };
 
     const goToCurrentMonth = () => {
       monthStartDate.value = startOfMonth(new Date());
-      updateRouterPath(monthStartDate.value as Date);
     };
 
     const triggerPrint = () => {
