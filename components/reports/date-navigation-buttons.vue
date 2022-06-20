1825c97c74cf392a23cf4abe424503edd0b488f6
Sander de Jong <sander@frontmen.nl>
2022-06-20 23:34:49 +0200

    fix: date not changing when switching months/years

--- components/reports/date-navigation-buttons.vue
+++ components/reports/date-navigation-buttons.vue
@@ -1,13 +1,13 @@
 <i18n lang="yaml">
-  en:
-    previousHint: "Or use keyboard left to go to previous {DATE_MSG}"
-    nextHint: "Or use keyboard right to go to next {DATE_MSG}"
-    currentHint: "To current {DATE_MSG}"
+en:
+  previousHint: "Or use keyboard left to go to previous {DATE_MSG}"
+  nextHint: "Or use keyboard right to go to next {DATE_MSG}"
+  currentHint: "To current {DATE_MSG}"
 
-  nl:
-    previousHint: "Or use keyboard left to go to previous {DATE_MSG}"
-    nextHint: "Or use keyboard right to go to next {DATE_MSG}"
-    currentHint: "Huidige {DATE_MSG}"
+nl:
+  previousHint: "Or use keyboard left to go to previous {DATE_MSG}"
+  nextHint: "Or use keyboard right to go to next {DATE_MSG}"
+  currentHint: "Huidige {DATE_MSG}"
 </i18n>
 
 <template>
@@ -41,15 +41,15 @@
       </b-button>
 
       <h2 class="navigation-buttons__week-label--reports text-capitalize">
-        {{YEAR_OR_MONTH_MSG}}
+        {{ YEAR_OR_MONTH_MSG }}
       </h2>
     </div>
   </div>
 </template>
 
 <script lang="ts">
-import {computed, defineComponent, onBeforeMount, ref, useContext} from '@nuxtjs/composition-api';
-import {differenceInCalendarMonths, getYear} from 'date-fns';
+import {computed, defineComponent, onBeforeMount, useContext} from '@nuxtjs/composition-api';
+import {differenceInCalendarMonths} from 'date-fns';
 import hotkeys from 'hotkeys-js';
 
 export default defineComponent({
@@ -66,19 +66,22 @@ export default defineComponent({
   emits: ['previous', 'next', 'current'],
   setup(props, {emit}) {
     const {i18n} = useContext();
-    const {selectedDate, isYearly} = props;
+
     const DATE_MSG = computed(() => {
-      const MSG = isYearly ? i18n.t('year') : i18n.t('month');
+      const MSG = props.isYearly ? i18n.t('year') : i18n.t('month');
       return `${MSG}`.toLowerCase();
     });
+
     const YEAR_OR_MONTH_MSG = computed(() => {
-      return isYearly ? selectedDate.getFullYear() : i18n.d(selectedDate, 'monthYear');
+      return props.isYearly ? props.selectedDate.getFullYear() : i18n.d(props.selectedDate, 'monthYear');
     });
+
     const handlePreviousClick = () => emit('previous');
     const handleNextClick = () => emit('next');
     const handleCurrentClick = () => {
       if (monthDifference.value !== 0) emit('current');
     };
+
     const monthDifference = computed(() => {
       const today = new Date();
       return differenceInCalendarMonths(props.selectedDate, today);
@@ -90,7 +93,6 @@ export default defineComponent({
     });
 
     return {
-      year,
       handlePreviousClick,
       handleNextClick,
       handleCurrentClick,
