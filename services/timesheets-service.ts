166e457f2d59731d87d19ed44c3b9dc2c9c2ed0d
Massimiliano Scifo <massimiliano.scifo@iodigital.com>
2022-08-12 15:29:29 +0200

    fix: moving db call from client to serverless function

--- services/timesheets-service.ts
+++ services/timesheets-service.ts
@@ -1,6 +1,4 @@
-import firebase from 'firebase/compat';
 import type {NuxtFireInstance} from '@nuxtjs/firebase';
-import {DocumentData} from '@firebase/firestore-types';
 import {Collections} from '~/types/enums';
 
 import {recordStatus} from '~/helpers/record-status';
@@ -12,32 +10,6 @@ export default class TimesheetsService {
   constructor(fire: NuxtFireInstance) {
     this.fire = fire;
     this.isServer = process.server;
-  }
-
-  async getTimesheets({date, startDate, endDate, employeeId}: GetTimesheetsProps) {
-    // Only do firebase calls on client side
-    if (this.isServer) return [];
-
-    let query:
-      | firebase.firestore.CollectionReference<DocumentData>
-      | firebase.firestore.Query<DocumentData> = this.fire.firestore.collection(
-      Collections.TIMESHEETS
-    );
-
-    if (date && !startDate && !endDate) query = query.where('date', '==', date);
-
-    if (startDate && !date) query = query.where('date', '>=', new Date(startDate).getTime());
-
-    if (endDate && !date) query = query.where('date', '<=', new Date(endDate).getTime());
-
-    if (employeeId) query = query.where('employeeId', '==', employeeId);
-
-    const snapshot = await query.get();
-
-    return snapshot.docs.map(doc => ({
-      id: doc.id,
-      ...(doc.data() as Omit<Timesheet, 'id'>),
-    }));
   }
 
   async getApprovedTimesheets(params: {startDate: number; endDate: number}): Promise<Timesheet[]> {
