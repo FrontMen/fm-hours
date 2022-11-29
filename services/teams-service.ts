a4d5e3e20cd0d394a15e2db4c41245dbba4ce341
Sander de Jong <sander.dejong@iodigital.com>
2022-11-29 14:20:30 +0100

    chore: Remove migration code

--- services/teams-service.ts
+++ services/teams-service.ts
@@ -15,46 +15,13 @@ export default class TeamsService {
     const ref = this.fire.firestore.collection(Collections.TEAMS);
     const snapshot = await ref.get();
 
-    // TEAMS MIGRATION
-    if (snapshot.docs.length === 1) {
-      // Create new teams
-      const teams = snapshot.docs[0].data().teams;
-      const newTeams: Team[] = [];
-      for (const doc of teams) {
-        const team = await this.add({name: doc});
-        newTeams.push(team);
-      }
-
-      // set id's of teams on employees
-      const empRef = this.fire.firestore.collection(Collections.EMPLOYEES);
-      const empSnapshot = await empRef.get();
-
-      for (const emp of empSnapshot.docs) {
-        const employee = emp.data();
-
-        const teamId = newTeams.find(a => a.name === employee.team)?.id || '';
-        await empRef.doc(emp.id).set(
-          {
-            ...employee,
-            team: teamId,
-            picture: this.fireModule.firestore.FieldValue.delete(),
-          },
-          {merge: true}
-        );
-      }
-
-      return newTeams;
-    }
-
-    return snapshot.docs
-      .filter((res: any) => res.data().name) // TODO: remove after migration
-      .map((res: any) => {
-        const team = res.data();
-        return {
-          id: res.id,
-          ...team,
-        } as Team;
-      });
+    return snapshot.docs.map((res: any) => {
+      const team = res.data();
+      return {
+        id: res.id,
+        ...team,
+      } as Team;
+    });
   }
 
   async add(params: Omit<Team, 'id' | 'createdAt'>): Promise<Team> {
