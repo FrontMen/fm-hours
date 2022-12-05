75e65e800a33bd87ce74b8ab58e48351d5aa59d4
Sander de Jong <sander.dejong@iodigital.com>
2022-12-05 11:54:54 +0100

    chore: cleanup admin pages

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -33,7 +33,7 @@ export default {
   // Auto import components (https://go.nuxtjs.dev/config-components)
   components: {
     dirs: [
-      '~/components/*',
+      '~/components',
       '~/components/admin',
       '~/components/app',
       '~/components/records',
