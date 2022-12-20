7a2f32188ec5520ed0f9c5670110b5726da4975d
Lee Ravenberg <lee@ravenberg.dev>
2022-12-20 15:41:48 +0100

    fix: improved logic structure in interceptor

--- lib/axios.ts
+++ lib/axios.ts
@@ -4,13 +4,13 @@ const instance = axios.create();
 
 instance.interceptors.request.use(
   requestConfig => {
-    if (axios.getUri(requestConfig).includes(process.env.BRIDGE_URL as string)) {
-      // Cancel bridge axios requests unless...
-      if (process.env.NODE_ENV !== 'development') return requestConfig;
-      if (requestConfig.method === 'get' || requestConfig.method === 'GET') return requestConfig;
-      if (axios.getUri(requestConfig).includes(process.env.AUTH_URL as string))
-        return requestConfig;
+    const uri = axios.getUri(requestConfig);
+    const isDev = process.env.NODE_ENV === 'development';
+    const isGet = requestConfig.method?.toLowerCase() === 'get';
+    const isBridgeUrl = uri.includes(process.env.BRIDGE_URL as string);
+    const isAuthUrl = uri.includes(process.env.AUTH_URL as string);
 
+    if (isDev && isGet && isBridgeUrl && !isAuthUrl) {
       throw new axios.Cancel(
         `Not sending '${requestConfig.method}' requests to Bridge production during development.`
       );
